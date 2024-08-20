af9d4ec82ec27168cb9559f682476c21847ddbe4
Robert Starsi <klevo@klevo.sk>
2024-08-20 15:53:47 +0200

    deploy task that reads current commit information

--- lib/envirobly/cli/main.rb
+++ lib/envirobly/cli/main.rb
@@ -1,6 +1,23 @@
+require "time"
+require "json"
+
 class Envirobly::Cli::Main < Envirobly::Base
   desc "version", "Show Envirobly CLI version"
   def version
     puts Envirobly::VERSION
+  end
+
+  desc "deploy ENVIRONMENT_NAME", "Deploy current commit to an environment"
+  def deploy(environ_name)
+    $stderr.puts "deploy to #{environ_name}"
+    commit_ref = `git rev-parse HEAD`.chomp("")
+    commit_message = `git log -1 --pretty=%B`.chomp("")
+    commit_time = Time.parse `git log -1 --date=iso --pretty=format:"%ad"`
+    commit = {
+      ref: commit_ref,
+      message: commit_message,
+      time: commit_time
+    }
+    puts commit.to_json
   end
 end
