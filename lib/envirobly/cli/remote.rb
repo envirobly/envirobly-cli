f713a3347f92ba6d86d7270e421585f2e8e80616
Robert Starsi <klevo@klevo.sk>
2024-08-22 12:24:14 +0200

    Reading .envirobly/project.yml

--- lib/envirobly/cli/remote.rb
+++ lib/envirobly/cli/remote.rb
@@ -1,5 +1,13 @@
+require "yaml"
+require "json"
+
 class Envirobly::Cli::Remote < Envirobly::Base
   URL_MATCHER = /^https:\/\/envirobly\.test\/(\d+)\/projects\/(\d+)$/
+  CONFIG_PATH = ".envirobly/project.yml"
+  DEFAULT_CONFIG = <<~YAML
+    remote:
+      origin: %origin_url%
+  YAML
 
   desc "add NAME URL", "Add a remote (connect a project by URL)"
   def add(name, url)
@@ -8,11 +16,20 @@ class Envirobly::Cli::Remote < Envirobly::Base
       exit 1
     end
 
-    puts "TODO add remote #{name} #{url}"
+    # $stderr.puts "add remote #{name} #{url}"
+
+    puts config.to_json
   end
 
   desc "show", "Show connected projects"
   def show
     puts "TODO list remotes"
   end
+
+  private
+    def config
+      YAML.load_file CONFIG_PATH
+    rescue Errno::ENOENT
+      DEFAULT_CONFIG
+    end
 end
