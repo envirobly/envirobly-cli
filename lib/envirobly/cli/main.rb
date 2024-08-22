6a31788bfa56b16435b7ac0696af83474bfa41ee
Robert Starsi <klevo@klevo.sk>
2024-08-22 12:25:31 +0200

    Remove Envirobly::Cli::Remote, this will not be part of launch functionality

--- lib/envirobly/cli/main.rb
+++ lib/envirobly/cli/main.rb
@@ -11,7 +11,4 @@ class Envirobly::Cli::Main < Envirobly::Base
   def deploy(environment)
     Envirobly::Deployment.new environment, options
   end
-
-  desc "remote", "Manage remotes (connected projects)"
-  subcommand "remote", Envirobly::Cli::Remote
 end
