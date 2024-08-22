db9e728162d4e6a4e586cca900dae17e25137e87
Robert Starsi <klevo@klevo.sk>
2024-08-22 10:29:09 +0200

    refacotring into Envirobly::Deployment

--- lib/envirobly/cli/main.rb
+++ lib/envirobly/cli/main.rb
@@ -9,44 +9,6 @@ class Envirobly::Cli::Main < Envirobly::Base
   desc "deploy ENVIRONMENT_NAME", "Deploy current commit to an environment"
   method_option :commit, type: :string, default: "HEAD"
   def deploy(environment)
-    @commit = Envirobly::Git::Commit.new options.commit
-
-    unless @commit.exists?
-      $stderr.puts "Commit #{options.commit} doesn't exist in this repository. Aborting."
-      exit 1
-    end
-
-    deployment_params = {
-      environ: {
-        name: environment
-      },
-      commit: {
-        ref: @commit.ref,
-        time: @commit.time,
-        message: @commit.message
-      }
-    }
-
-    api = Envirobly::Api.new
-    response = api.create_deployment deployment_params
-    @credentials = Envirobly::Aws::Credentials.new response.object.fetch("credentials")
-    @bucket = response.object.fetch("bucket")
-
-    if archive_build_context
-      $stderr.puts "Build context exported into #{archive_uri}"
-    else
-      $stderr.puts "Error exporting build context. Aborting."
-      exit 1
-    end
+    Envirobly::Deployment.new environment, options
   end
-
-  private
-    def archive_uri
-      "s3://#{@bucket}/#{@commit.ref}.tar.gz"
-    end
-
-    def archive_build_context
-      `git archive --format=tar.gz #{@commit.ref} | #{@credentials.as_inline_env_vars} aws s3 cp - #{archive_uri}`
-      $?.success?
-    end
 end
--- lib/envirobly/deployment.rb
+++ lib/envirobly/deployment.rb
@@ -1,4 +1,43 @@
 class Envirobly::Deployment
-  def initialize
+  def initialize(environment, options)
+    @commit = Envirobly::Git::Commit.new options.commit
+
+    unless @commit.exists?
+      $stderr.puts "Commit #{options.commit} doesn't exist in this repository. Aborting."
+      exit 1
+    end
+
+    deployment_params = {
+      environ: {
+        name: environment
+      },
+      commit: {
+        ref: @commit.ref,
+        time: @commit.time,
+        message: @commit.message
+      }
+    }
+
+    api = Envirobly::Api.new
+    response = api.create_deployment deployment_params
+    @credentials = Envirobly::Aws::Credentials.new response.object.fetch("credentials")
+    @bucket = response.object.fetch("bucket")
+
+    if archive_commit_and_upload
+      $stderr.puts "Build context exported into #{archive_uri}"
+    else
+      $stderr.puts "Error exporting build context. Aborting."
+      exit 1
+    end
   end
+
+  private
+    def archive_uri
+      "s3://#{@bucket}/#{@commit.ref}.tar.gz"
+    end
+
+    def archive_commit_and_upload
+      `git archive --format=tar.gz #{@commit.ref} | #{@credentials.as_inline_env_vars} aws s3 cp - #{archive_uri}`
+      $?.success?
+    end
 end
