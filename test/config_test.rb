408537589e325d9c6e583fda02ef26e4b5f1fa37
Robert Starsi <klevo@klevo.sk>
2024-09-18 15:48:32 +0200

    preparing environment override data

--- test/config_test.rb
+++ test/config_test.rb
@@ -4,7 +4,7 @@ class Envirobly::ConfigTest < ActiveSupport::TestCase
   test "parsing" do
     commit = Envirobly::Git::Commit.new("38541a424ac370a6cccb4a4131f1125a7535cb84", working_dir:)
     config = Envirobly::Config.new commit
-    assert_equal config_yml, config.raw
+    assert_equal simple_config_yml, config.raw
     expected_hash = {
       services: {
         db: {
@@ -20,7 +20,7 @@ class Envirobly::ConfigTest < ActiveSupport::TestCase
     assert_equal expected_hash, config.to_h
   end
 
-  def config_yml
+  def simple_config_yml
     <<~YAML
       services:
         db:
@@ -28,6 +28,70 @@ class Envirobly::ConfigTest < ActiveSupport::TestCase
           instance_type: t4g.small
         app:
           dockerfile: Dockerfile
+    YAML
+  end
+
+  def kitchen_sink_config_yml
+    <<~YAML
+      services:
+        pg:
+          name: Elephant
+          type: postgres
+          engine_version: 16.0
+          instance_type: t4g.nano
+          disk_size: 25
+        mysql:
+          name: Sun ☀️
+          type: mysql
+          engine_version: 8.1
+          instance_type: t4g.nano
+          disk_size: 30
+        app:
+          name: SuperApp
+          dockerfile: Dockerfile.production
+          build_context: app
+          env:
+            RAILS_MASTER_KEY:
+              file: config/master.key
+            WEATHER: sunny
+            RAILS_ENV: preview
+            DATABASE_URL:
+              service: pg
+              key: url
+          instance_type: t4g.small
+          health_check_path: /up
+        blog:
+          image: wordpress
+          env:
+            DATABASE_HOST:
+              service: mysql
+              key: host
+            DATABASE_NAME:
+              service: mysql
+              key: name
+          volume_mount_path: /usr/public/html
+          disk_size: 30
+
+      environments:
+        production:
+          pg:
+            instance_type: t4g.large
+            disk_size: 400
+          mysql:
+            instance_type: t4g.2xlarge
+            disk_size: 500
+          app:
+            name: SuperApp Production
+            dockerfile: Dockerfile.production
+            build_context: app
+            env:
+              RAILS_MASTER_KEY:
+                WEATHER: null
+                RAILS_ENV: production
+                RAILS_MAX_THREADS: 5
+            instance_type: t4g.medium
+            min_instances: 2
+            max_instances: 4
     YAML
   end
 end
