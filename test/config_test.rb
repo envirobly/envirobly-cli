c6957bd298c04fbad55ff261f359a2284eec4a28
Robert Starsi <klevo@klevo.sk>
2024-09-18 16:32:40 +0200

    refactoring

--- test/config_test.rb
+++ test/config_test.rb
@@ -5,19 +5,7 @@ class Envirobly::ConfigTest < ActiveSupport::TestCase
     commit = Envirobly::Git::Commit.new("38541a424ac370a6cccb4a4131f1125a7535cb84", working_dir:)
     config = Envirobly::Config.new commit
     assert_equal simple_config_yml, config.raw
-    expected_hash = {
-      services: {
-        db: {
-          type: "postgres",
-          instance_type: "t4g.small"
-        },
-        app: {
-          dockerfile: "Dockerfile",
-          image_tag: "645842777640896aa3d68f2573be5d33571b5001"
-        }
-      }
-    }
-    assert_equal expected_hash, config.to_h
+    assert_equal simple_config, config.to_h
   end
 
   test "parsing kitchen sink config" do
@@ -36,6 +24,21 @@ class Envirobly::ConfigTest < ActiveSupport::TestCase
         app:
           dockerfile: Dockerfile
     YAML
+  end
+
+  def simple_config
+    {
+      services: {
+        db: {
+          type: "postgres",
+          instance_type: "t4g.small"
+        },
+        app: {
+          dockerfile: "Dockerfile",
+          image_tag: "645842777640896aa3d68f2573be5d33571b5001"
+        }
+      }
+    }
   end
 
   def kitchen_sink_config_yml
