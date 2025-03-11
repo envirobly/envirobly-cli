22e03ad276fcd27859e2774503774deefc88435c
Robert Starsi <klevo@klevo.sk>
2025-03-11 16:45:14 +0100

    listing all objects to check what needs to be uploaded efficiently

--- lib/envirobly/aws/s3.rb
+++ lib/envirobly/aws/s3.rb
@@ -5,6 +5,8 @@ require "concurrent"
 require "benchmark"
 
 class Envirobly::Aws::S3
+  OBJECTS_PREFIX = "git-objects"
+
   def initialize(bucket)
     @bucket = bucket
     @client = Aws::S3::Client.new # TODO: region and creds should be passed
@@ -13,15 +15,34 @@ class Envirobly::Aws::S3
   def push(commit)
     puts "Pushing commit #{commit.ref} to #{@bucket}"
 
-    object_hashes = commit.object_tree.map { |object| object[:hash] }
+    remote_object_hashes = list_object_hashes
+    local_object_hashes = commit.object_tree.map { |object| object[:hash] }
+    object_hashes_to_upload = local_object_hashes - remote_object_hashes
+
+    puts "Uploading #{object_hashes_to_upload.size} out of #{local_object_hashes.size}"
 
     timings = Benchmark.measure do
-      upload_git_objects object_hashes
+      upload_git_objects object_hashes_to_upload
     end
-    puts "Took #{format_duration timings.real}"
+    puts "Done in #{format_duration timings.real}"
   end
 
   private
+    def list_object_hashes
+      @client.list_objects({
+        bucket: @bucket,
+        prefix: "#{OBJECTS_PREFIX}/"
+      }).map do |response|
+        response.contents.map do |object|
+          object.key.delete_prefix("#{OBJECTS_PREFIX}/").delete_suffix(".gz")
+        end
+      end.flatten
+    end
+
+    def object_hash_to_key(object_hash)
+      "#{OBJECTS_PREFIX}/#{object_hash}.gz"
+    end
+
     def object_exists?(key)
       @client.head_object(bucket: @bucket, key:)
       true
@@ -29,29 +50,22 @@ class Envirobly::Aws::S3
       false
     end
 
-    def compress_and_upload_object(git_object_hash)
-      key = "git-objects/#{git_object_hash}.gz"
-
-      # TODO: Replace with list objects first, then only uploading the ones that don't
-      if object_exists?(key)
-        puts "✔ #{key}"
-        return
-      end
+    def compress_and_upload_object(object_hash)
+      key = object_hash_to_key object_hash
 
       Tempfile.create(["envirobly-push", ".gz"]) do |tempfile|
         Zlib::GzipWriter.new(tempfile) do |gz|
-          Open3.popen3("git", "cat-file", "-p", git_object_hash) do |_, stdout, stderr, thread|
+          Open3.popen3("git", "cat-file", "-p", object_hash) do |_, stdout, stderr, thread|
             IO.copy_stream(stdout, gz)
 
             unless thread.value.success?
-              raise "`git cat-file -p #{git_object_hash}` failed: #{stderr.read}"
+              raise "`git cat-file -p #{object_hash}` failed: #{stderr.read}"
             end
           end
         end
 
+        @client.put_object(bucket: @bucket, body: tempfile, key:)
 
-        resp = @client.put_object(bucket: @bucket, body: tempfile, key:)
-        # puts resp.to_h
         puts "⤴ #{key}"
       end
     end
