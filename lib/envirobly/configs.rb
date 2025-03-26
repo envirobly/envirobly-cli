c66f89575800b9859567ea6eab9fe404317f6090
Robert Starsi <klevo@klevo.sk>
2025-03-26 10:04:26 +0100

    to_params with env_vars

--- lib/envirobly/configs.rb
+++ lib/envirobly/configs.rb
@@ -2,14 +2,22 @@ require "dotenv"
 
 class Envirobly::Configs
   DIR = ".envirobly"
+  ENV = "env"
   BASE = "deploy.yml"
   OVERRIDES_PATTERN = /deploy\.([a-z0-9\-_]+)\.yml/i
 
   def initialize(dir = DIR)
-
+    @dir = Pathname.new dir
   end
 
-  def env_vars
-    @env_vars ||= Dotenv.parse(ENV_PATH)
+  def to_params
+    {
+      env_vars:
+    }
   end
+
+  private
+    def env_vars
+      Dotenv.parse @dir.join(ENV)
+    end
 end
