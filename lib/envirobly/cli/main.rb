e9b3ffa4902eef6b001840c57eac4c602ba52918
Robert Starsi <klevo@klevo.sk>
2025-07-21 18:04:52 +0200

    connecting

--- lib/envirobly/cli/main.rb
+++ lib/envirobly/cli/main.rb
@@ -126,7 +126,7 @@ class Envirobly::Cli::Main < Envirobly::Base
   method_option :shell, type: :string
   method_option :user, type: :string
   def exec(service_name, *command)
-    say "exec #{service_name} #{command.join(" ")}"
+    # say "exec #{service_name} #{command.join(" ")}"
 
     api = Envirobly::Api.new
     params = {
@@ -136,6 +136,41 @@ class Envirobly::Cli::Main < Envirobly::Base
       instance: { slot: options.instance_slot }
     }
     response = api.create_service_shell_connection params
-    puts response.object
+    ssh_params = response.object
+
+    cmd_template =
+      "AWS_ACCESS_KEY_ID='%s' " +
+      "AWS_SECRET_ACCESS_KEY='%s' " +
+      "AWS_SESSION_TOKEN='%s' " +
+      # "ENVIROBLY_SERVICE_SHELL_USER=root " +
+      # "ENVIROBLY_SERVICE_INTERACTIVE_SHELL=bash " +
+      "ssh -i %s " +
+      "envirobly-service@%s " +
+      # "-o SendEnv=ENVIROBLY_SERVICE_SHELL_USER " +
+      # "-o SendEnv=ENVIROBLY_SERVICE_INTERACTIVE_SHELL " +
+      "-o StrictHostKeyChecking=accept-new " +
+      "-o ProxyCommand='aws ec2-instance-connect open-tunnel --instance-id %s --region %s'"
+
+    Tempfile.create do |tempkey|
+      tempkey.write ssh_params.fetch("instance").fetch("private_key")
+      tempkey.flush
+
+      cmd = sprintf(
+        cmd_template,
+        ssh_params.fetch("open_tunnel_credentials").fetch("access_key_id"),
+        ssh_params.fetch("open_tunnel_credentials").fetch("secret_access_key"),
+        ssh_params.fetch("open_tunnel_credentials").fetch("session_token"),
+        tempkey.path,
+        ssh_params.fetch("instance").fetch("private_ipv4"),
+        ssh_params.fetch("instance").fetch("aws_id"),
+        ssh_params.fetch("region")
+      )
+
+      if command.present?
+        cmd = %(#{cmd} "#{command.join(" ")}")
+      end
+
+      system cmd
+    end
   end
 end
