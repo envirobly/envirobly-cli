62d6f29ad65e236b9f84520e78791ba9c429a146
Robert Starsi <klevo@klevo.sk>
2025-07-24 09:16:05 +0200

    refactoring

--- lib/envirobly/container_shell.rb
+++ lib/envirobly/container_shell.rb
@@ -1,17 +1,23 @@
 class Envirobly::ContainerShell
-  SSH_TEMPLATE =
-    "AWS_ACCESS_KEY_ID='%s' " +
-    "AWS_SECRET_ACCESS_KEY='%s' " +
-    "AWS_SESSION_TOKEN='%s' " +
-    "ssh -i %s " +
-    "envirobly-service@%s " +
-    "-o StrictHostKeyChecking=accept-new " +
+  ENV = [
+    "AWS_ACCESS_KEY_ID='%s'",
+    "AWS_SECRET_ACCESS_KEY='%s'",
+    "AWS_SESSION_TOKEN='%s'"
+  ]
+  SSH = [
+    "ssh -i %s",
+    "-o StrictHostKeyChecking=accept-new",
+    "-o SendEnv=ENVIROBLY_SERVICE_INTERACTIVE_SHELL",
+    "-o SendEnv=ENVIROBLY_SERVICE_SHELL_USER",
     "-o ProxyCommand='aws ec2-instance-connect open-tunnel --instance-id %s --region %s'"
+  ]
+  USER_AND_HOST = "envirobly-service@%s"
 
-  def initialize(service_name, command, options)
-    @command = command
+  def initialize(service_name, inner_command, options)
+    @inner_command = inner_command
     @options = options
     commit = Envirobly::Git::Commit.new "HEAD"
+
     @params = {
       project: {
         account_id: options.account_id || Envirobly::Defaults::Account.new.id,
@@ -29,38 +35,52 @@ class Envirobly::ContainerShell
   end
 
   def connect
-    api = Envirobly::Api.new
-    response = api.create_service_shell_connection @params
-    ssh_params = response.object
-
-    Tempfile.create do |tempkey|
-      tempkey.write ssh_params.fetch("instance").fetch("private_key")
-      tempkey.flush
-
+    with_private_key do |private_key_path|
       cmd = sprintf(
-        SSH_TEMPLATE,
+        join(ENV, SSH, USER_AND_HOST),
         ssh_params.fetch("open_tunnel_credentials").fetch("access_key_id"),
         ssh_params.fetch("open_tunnel_credentials").fetch("secret_access_key"),
         ssh_params.fetch("open_tunnel_credentials").fetch("session_token"),
-        tempkey.path,
-        ssh_params.fetch("instance").fetch("private_ipv4"),
+        private_key_path,
         ssh_params.fetch("instance").fetch("aws_id"),
-        ssh_params.fetch("region")
+        ssh_params.fetch("region"),
+        ssh_params.fetch("instance").fetch("private_ipv4")
       )
 
       if @options.shell.present?
-        cmd = "ENVIROBLY_SERVICE_INTERACTIVE_SHELL='#{@options.shell}' #{cmd} -o SendEnv=ENVIROBLY_SERVICE_INTERACTIVE_SHELL"
+        cmd = join "ENVIROBLY_SERVICE_INTERACTIVE_SHELL='#{@options.shell}'", cmd
       end
 
       if @options.user.present?
-        cmd = "ENVIROBLY_SERVICE_SHELL_USER='#{@options.user}' #{cmd} -o SendEnv=ENVIROBLY_SERVICE_SHELL_USER"
+        cmd = join "ENVIROBLY_SERVICE_SHELL_USER='#{@options.user}'", cmd
       end
 
-      if @command.present?
-        cmd = "#{cmd} #{@command.join(" ")}"
+      if @inner_command.present?
+        cmd = join cmd, @inner_command
       end
 
       system cmd
     end
   end
+
+  private
+    def join(*parts)
+      parts.flatten.join(" ")
+    end
+
+    def ssh_params
+      @ssh_params ||= begin
+        api = Envirobly::Api.new
+        api.create_service_shell_connection(@params).object
+      end
+    end
+
+    def with_private_key
+      Tempfile.create do |file|
+        file.write ssh_params.fetch("instance").fetch("private_key")
+        file.flush
+
+        yield file.path
+      end
+    end
 end
