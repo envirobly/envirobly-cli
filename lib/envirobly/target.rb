c9e7fc413fc356b03f19fe80e2920635430f220f
Robert Starsi <klevo@klevo.sk>
2025-09-11 15:52:06 +0200

    implementing

---
+++ lib/envirobly/target.rb
@@ -0,0 +1,40 @@
+# frozen_string_literal: true
+
+module Envirobly
+  class Target
+    attr_accessor :account_id, :project_id, :region
+
+    def initialize(
+        default_account_id: nil,
+        default_project_id: nil,
+        default_region: nil,
+        account_id: nil,
+        project_id: nil
+      )
+      @default_account_id = default_account_id
+      @default_project_id = default_project_id
+      @default_region = default_region
+      @account_id = account_id
+    end
+
+    def missing_params
+      []
+    end
+
+    def account_id
+      return if @project_id
+
+      @account_id || @default_account_id
+    end
+
+    def project_id
+      return if @account_id
+
+      @project_id || @default_project_id
+    end
+
+    def region
+      @region || @default_region
+    end
+  end
+end
